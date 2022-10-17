64194dbc51ecf20470d1d3f0d85e2cfab6a393a2
Natalia.Codes <nataliadlr.codes@gmail.com>
2022-10-17 20:17:29 -0300

    subiendo primeros archivos adopciones

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scroll down ease in out Function -- completar //
+
+// Popup Form //
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
